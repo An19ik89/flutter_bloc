33c48215bc8ef520e598fce36e882966b57c818d
Anik <sajjad@royex.net>
2021-06-19 16:57:15 +0600

    bloc added 2

--- lib/counterBloc.dart
+++ lib/counterBloc.dart
@@ -1,9 +1,45 @@
 import 'dart:async';
 
-class CounterBloc{
-  final _stateStreamController = StreamController<int>();
+enum counterAction{
+  Increment,
+  Decrement,
+  Clear
+}
 
+class CounterBloc
+{
+  int counter;
+  final _stateStreamController = StreamController<int>();
   StreamSink<int> get counterSink => _stateStreamController.sink;
   Stream<int> get counterStream => _stateStreamController.stream;
 
+  final _eventStreamController = StreamController<counterAction>();
+  StreamSink<counterAction> get eventCounterSink => _eventStreamController.sink;
+  Stream<counterAction> get eventCounterStream => _eventStreamController.stream;
+
+  CounterBloc()
+  {
+    counter = 0;
+    eventCounterStream.listen((event) {
+      if(event == counterAction.Increment){
+        counter++;
+      }
+      else if(event == counterAction.Decrement){
+        if(counter != 0){
+          counter--;
+        }
+      }
+      else{
+        counter = 0;
+      }
+
+      counterSink.add(counter);
+    });
+
+  }
+
+  void dispose(){
+    _stateStreamController.close();
+    _eventStreamController.close();
+  }
 }
